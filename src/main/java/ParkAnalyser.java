f7c978ae59642180b87b2f23e128ec27ad8d3404
prasadpalkar94 <prasadpalkar94@gmail.com>
2020-04-02 20:44:14 +0530

    ADDED:Check Condition For UnPark Car

--- src/main/java/ParkAnalyser.java
+++ src/main/java/ParkAnalyser.java
@@ -1,6 +1,18 @@
 public class ParkAnalyser {
 
+    boolean unPark = false;
+
     public boolean parkCar() {
         return true;
+    }
+
+    boolean park = parkCar();
+
+    public boolean unParkCar() {
+        if ( park == true )
+        {
+            unPark = true;
+        }
+        return unPark;
     }
 }
--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,11 +1,24 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest {
+    ParkAnalyser parkAnalyser;
+
+    @Before
+    public void setUp() throws Exception {
+        parkAnalyser = new ParkAnalyser();
+    }
+
     @Test
     public void checkConditionFor_ParkTheCar_ShouldReturnTrue() {
-        ParkAnalyser parkAnalyser = new ParkAnalyser();
         boolean check = parkAnalyser.parkCar();
+        Assert.assertEquals(true,check);
+    }
+
+    @Test
+    public void checkConditionFor_UnParkTheCar_ShouldReturnTrue() {
+        boolean check = parkAnalyser.unParkCar();
         Assert.assertEquals(true,check);
     }
 }
